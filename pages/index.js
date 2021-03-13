f6558acd9152f781daa61202e06355f2569e3c7f
Hiroki Matsuda <h.matsu0330@gmail.com>
2021-03-13 15:21:36 +0900

    "Dynamic Routes" まで

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 import Head from 'next/head'
+import Link from 'next/link'
 import Layout, { siteTitle } from '../components/layout'
 import { getSortedPostsData } from '../lib/posts'
 import utilStyles from '../styles/utils.module.css'
+import Date from "../components/date";
 
 export default function Home({ allPostsData }) {
   return (
@@ -23,11 +25,13 @@ export default function Home({ allPostsData }) {
                 {
                     allPostsData.map(({id, date, title}) => (
                         <li className={utilStyles.listItem} key={id}>
-                            {title}
+                            <Link href={`/posts/${id}`}>
+                                <a>{title}</a>
+                            </Link>
                             <br/>
-                            {id}
-                            <br/>
-                            {date}
+                            <small className={utilStyles.lightText}>
+                                <Date dateString={date}/>
+                            </small>
                         </li>
                     ))
                 }
